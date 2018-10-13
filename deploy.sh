14da1901ab25f713c08c24aeb6b9de0863a8c939
John Higgins Avila <johnhiggins.avila@gmail.com>
2018-10-13 07:52:51 -0700

    id: ISSUES-64 G-Cloud Deployment (#24)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 docker build -t johnhigginsavila/web-app:latest -t johnhigginsavila/web-app:$SHA -f ./Dockerfile .
 docker push johnhigginsavila/web-app:latest
 docker push johnhigginsavila/web-app:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/web-app-deployment web-app=johnhigginsavila/web-app:$SHA
